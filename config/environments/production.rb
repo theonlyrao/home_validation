985098fb5397fc1520c84f7a24d52f605f3eb976
Ashwin Rao <theonlyrao@gmail.com>
2016-06-10 23:00:08 -0600

    Add comma to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
     s3_credentials: {
       bucket: ENV['AWS_S3_BUCKET'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       :s3_region => ENV['AWS_REGION']
     }
   }
