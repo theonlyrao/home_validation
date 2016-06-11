f9ae8c9861ce042c5c6aac4bca6511b92a425d1e
Ashwin Rao <theonlyrao@gmail.com>
2016-06-10 22:37:13 -0600

    Establish file upload and validation

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -6,14 +6,15 @@ class HomesController < ApplicationController
   end
 
   def create
-    validation = Creator.build(validation_params[:home], validation_params[:pictures_attributes]["0"])
+    raw_picture = params[:home][:pictures_attributes]["0"][:picture]
+    validation = Creator.build(validation_params[:home], raw_picture)
     redirect_to validation_show_path(validation.id)
   end
 
   private
 
   def validation_params
-    params.require(:home).permit(home: [:address_1, :city, :state, :zip], pictures_attributes: [:lat, :long] )
+    params.require(:home).permit(home: [:address_1, :city, :state, :zip])
     # file = params["home"]["pictures_attributes"]["0"]["picture"].tempfile
     # gps = EXIFR::JPEG.new(file).gps
   end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,8 @@ Rails.application.configure do
       bucket: ENV['AWS_S3_BUCKET'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-    }
+    },
+    :s3_region => ENV['AWS_REGION']
   }
   Paperclip.options[:command_path] = "/usr/local/bin/"
 
