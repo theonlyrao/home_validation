1197ae534c04178819083f9a812c5d0907b3aa5a
Ashwin Rao <theonlyrao@gmail.com>
2016-06-11 22:52:34 -0600

    Remove address_1 from code

--- spec/controllers/validation_controller_spec.rb
+++ spec/controllers/validation_controller_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe HomesController, type: :controller do
 
   it "can handle submission with bad address" do
     VCR.use_cassette "controller_with_bad_address" do
-      post :create, home: {home: {address_1: "aoi;dljf", city: "Deasdr", state: "CO", zip: "992313280"}, pictures_attributes: {"0" => {picture: Rack::Test::UploadedFile.new(File.join(ActionController::TestCase.fixture_path, 'turing3.jpg'), 'image/jpeg')}}}
+      post :create, home: {home: {address: "aoi;dljf", city: "Deasdr", state: "CO", zip: "992313280"}, pictures_attributes: {"0" => {picture: Rack::Test::UploadedFile.new(File.join(ActionController::TestCase.fixture_path, 'turing3.jpg'), 'image/jpeg')}}}
       assert_redirected_to validation_path
       expect(flash[:error]).to be_present
     end
@@ -20,7 +20,7 @@ RSpec.describe HomesController, type: :controller do
 
   it "can handle submission with incomplete but usable address" do
     VCR.use_cassette "controller_with_usable_address" do
-      post :create, home: {home: {address_1: "", city: "Denver", state: "CO", zip: "80202"}, pictures_attributes: {"0" => {picture: Rack::Test::UploadedFile.new(File.join(ActionController::TestCase.fixture_path, 'turing3.jpg'), 'image/jpeg')}}}
+      post :create, home: {home: {address: "", city: "Denver", state: "CO", zip: "80202"}, pictures_attributes: {"0" => {picture: Rack::Test::UploadedFile.new(File.join(ActionController::TestCase.fixture_path, 'turing3.jpg'), 'image/jpeg')}}}
       assert_redirected_to validation_show_path(Validation.last.id)
       expect(Validation.last.validated?).to eq(false)
     end
@@ -28,7 +28,7 @@ RSpec.describe HomesController, type: :controller do
 
   it "can handle submission with no picture" do
     VCR.use_cassette "controller_with_no_picture" do
-      post :create, home: {home: {address_1: "1501 Blake Street", city: "Denver", state: "CO", zip: "80202"}}
+      post :create, home: {home: {address: "1501 Blake Street", city: "Denver", state: "CO", zip: "80202"}}
       assert_redirected_to validation_path
       expect(flash[:error]).to be_present
     end
@@ -36,7 +36,7 @@ RSpec.describe HomesController, type: :controller do
 
   it "can handle submission with no gps metadata" do
     VCR.use_cassette "controller_with_no_gps_metadata" do
-      post :create, home: {home: {address_1: "3937 Tennyson St", city: "Denver", state: "CO", zip: "80212"}, pictures_attributes: {"0" => {picture: Rack::Test::UploadedFile.new(File.join(ActionController::TestCase.fixture_path, 'no_gps.jpg'), 'image/jpeg')}}}
+      post :create, home: {home: {address: "3937 Tennyson St", city: "Denver", state: "CO", zip: "80212"}, pictures_attributes: {"0" => {picture: Rack::Test::UploadedFile.new(File.join(ActionController::TestCase.fixture_path, 'no_gps.jpg'), 'image/jpeg')}}}
       assert_redirected_to validation_path
       expect(flash[:error]).to be_present
     end
@@ -44,7 +44,7 @@ RSpec.describe HomesController, type: :controller do
 
   it "can handle submission with good input" do
     VCR.use_cassette "controller_with_good_input" do
-      post :create, home: {home: {address_1: "1510 Blake Street", city: "Denver", state: "CO", zip: "80202"}, pictures_attributes: {"0" => {picture: Rack::Test::UploadedFile.new(File.join(ActionController::TestCase.fixture_path, 'turing3.jpg'), 'image/jpeg')}}}
+      post :create, home: {home: {address: "1510 Blake Street", city: "Denver", state: "CO", zip: "80202"}, pictures_attributes: {"0" => {picture: Rack::Test::UploadedFile.new(File.join(ActionController::TestCase.fixture_path, 'turing3.jpg'), 'image/jpeg')}}}
       expect(flash[:error]).not_to be_present
       assert_redirected_to validation_show_path(Validation.last.id)
     end
--- spec/models/home_spec.rb
+++ spec/models/home_spec.rb
@@ -7,14 +7,14 @@ RSpec.describe Home, type: :model do
 
   it "knows a bad address" do
     VCR.use_cassette("model_home_with_bad_address") do
-      result = Home.bad_address?(address_1: "adsflksjdf", city: "Deafdsr", zip: "8032402")
+      result = Home.bad_address?(address: "adsflksjdf", city: "Deafdsr", zip: "8032402")
       expect(result).to eq(true)
     end
   end
   
   it "creates new home with complete gmaps address results" do
     VCR.use_cassette("model_home_with_complete_gmaps_results") do
-      turing = Home.build_home(address_1: "1510 Blake Street", city: "Denver", zip: "80202")
+      turing = Home.build_home(address: "1510 Blake Street", city: "Denver", zip: "80202")
       expect(turing.lat).to eq("39.7496354")
       expect(turing.long).to eq("-105.0001058")
       expect(turing.address).to eq("1510 Blake St, Denver, CO 80202, USA")
