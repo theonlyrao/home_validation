e3a9feb68439b1de92c5e71f2ead7829614e8bd7
Ashwin Rao <theonlyrao@gmail.com>
2016-06-15 13:25:39 -0600

    Allow user with no validations to see dashboard
    Previously the page broke because the cacheing looks for the last
    validation belonging to the user. Now there is a helper method that will
    let all users be cached.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def cache_key_for(user)
+    return "user-#{user.id}-validations-0" unless user.validations.last
+    "user-#{user.id}-validations-#{user.validations.last.id}"
+  end
 end
