6d554389d687cdac19ccc580ca4fe4bc591adcbe
Ashwin Rao <theonlyrao@gmail.com>
2016-06-15 17:42:54 -0600

    Fix typo in omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'],
-           :callback_url, "/auth/facebook/callback"
+           callback_url: "/auth/facebook/callback"
 end
