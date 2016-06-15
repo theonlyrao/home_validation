7f9dfa148b5ed8b0c980995a7bf25ce063192f70
Ashwin Rao <theonlyrao@gmail.com>
2016-06-14 21:11:01 -0600

    Fix flash alerts
    Keys and flash class now changed to work with bootstrap. Also message added for
    successful logout.

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -8,10 +8,10 @@ class HomesController < ApplicationController
   def create
     if Home.bad_address?(validation_params[:home])
       redirect_to validation_path
-      flash[:error] = "Please check the address."
+      flash[:warning] = "Please check the address."
     elsif Picture.no_gps?(params[:home][:pictures_attributes])
       redirect_to validation_path
-      flash[:error] = "Please make sure you have attached a picture with gps metadata."
+      flash[:warning] = "Please make sure you have attached a picture with gps metadata."
     else
       validation = Creator.build(validation_params[:home], Picture.raw_picture, current_user)
       redirect_to validation_show_path(validation.id)
